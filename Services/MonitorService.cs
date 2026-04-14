850afd1eae4f369cc8e5f7a1a96f4ef32da3b6ec
silvia-hacks <tsukikagerow@gmail.com>
2026-04-14 21:56:03 +0900

    Run mail processing off the UI thread

--- Services/MonitorService.cs
+++ Services/MonitorService.cs
@@ -32,6 +32,8 @@ public sealed class MonitorService : IDisposable
 
     public bool IsRunning { get; private set; }
 
+    public bool IsCycleRunning { get; private set; }
+
     public DateTimeOffset? LastCheckAt { get; private set; }
 
     public DateTimeOffset? LastForwardAt { get; private set; }
@@ -68,36 +70,37 @@ public sealed class MonitorService : IDisposable
 
     public void ExecuteCycle()
     {
-        if (!_cycleLock.Wait(0))
-        {
-            return;
-        }
-
-        try
-        {
-            ExecuteCycleCore(limitToSingleMessage: false);
-        }
-        finally
-        {
-            _cycleLock.Release();
-        }
+        _ = ExecuteCycleAsync(limitToSingleMessage: false);
     }
 
-    public void ExecuteSingleCycle()
+    public Task ExecuteSingleCycleAsync()
+    {
+        return ExecuteCycleAsync(limitToSingleMessage: true);
+    }
+
+    private Task ExecuteCycleAsync(bool limitToSingleMessage)
     {
         if (!_cycleLock.Wait(0))
         {
-            return;
+            return Task.CompletedTask;
         }
 
-        try
+        IsCycleRunning = true;
+        RaiseStateChanged();
+
+        return Task.Run(() =>
         {
-            ExecuteCycleCore(limitToSingleMessage: true);
-        }
-        finally
-        {
-            _cycleLock.Release();
-        }
+            try
+            {
+                ExecuteCycleCore(limitToSingleMessage);
+            }
+            finally
+            {
+                IsCycleRunning = false;
+                RaiseStateChanged();
+                _cycleLock.Release();
+            }
+        });
     }
 
     private void ExecuteCycleCore(bool limitToSingleMessage)
--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -24,7 +24,7 @@ public sealed class MainViewModel : ViewModelBase
         StartMonitoringCommand = new RelayCommand(_ => StartMonitoring(), _ => !_monitorService.IsRunning);
         StopMonitoringCommand = new RelayCommand(_ => StopMonitoring(), _ => _monitorService.IsRunning);
         OpenSettingsCommand = new RelayCommand(_ => OpenSettingsRequested?.Invoke(this, EventArgs.Empty));
-        RunOnceCommand = new RelayCommand(_ => RunOnce());
+        RunOnceCommand = new RelayCommand(_ => RunOnce(), _ => !_monitorService.IsCycleRunning);
 
         _monitorService.StateChanged += (_, _) => RefreshState();
         RefreshState();
@@ -48,6 +48,10 @@ public sealed class MainViewModel : ViewModelBase
         private set => SetProperty(ref _statusText, value);
     }
 
+    public string ExecutionStatusText => _monitorService.IsCycleRunning ? "実行中..." : string.Empty;
+
+    public bool IsExecuting => _monitorService.IsCycleRunning;
+
     public string LastCheckText => _monitorService.LastCheckAt?.ToString("yyyy-MM-dd HH:mm:ss") ?? "-";
 
     public string LastForwardText => _monitorService.LastForwardAt?.ToString("yyyy-MM-dd HH:mm:ss") ?? "-";
@@ -66,18 +70,21 @@ public sealed class MainViewModel : ViewModelBase
         RefreshState();
     }
 
-    private void RunOnce()
+    private async void RunOnce()
     {
-        _monitorService.ExecuteSingleCycle();
+        await _monitorService.ExecuteSingleCycleAsync();
         RefreshState();
     }
 
     private void RefreshState()
     {
         StatusText = _monitorService.IsRunning ? "監視中" : "停止中";
+        OnPropertyChanged(nameof(ExecutionStatusText));
+        OnPropertyChanged(nameof(IsExecuting));
         OnPropertyChanged(nameof(LastCheckText));
         OnPropertyChanged(nameof(LastForwardText));
         StartMonitoringCommand.RaiseCanExecuteChanged();
         StopMonitoringCommand.RaiseCanExecuteChanged();
+        RunOnceCommand.RaiseCanExecuteChanged();
     }
 }
